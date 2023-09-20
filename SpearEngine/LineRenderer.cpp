66247b4659e75b7b2b6bf940b65532dc2a80ae29
tspiers@sumo-digital.com <tspiers@sumo-digital.com>
2023-09-20 23:16:39 +0100

    Refactored LineRenderer to support adding lines from within Game code

--- SpearEngine/LineRenderer.cpp
+++ SpearEngine/LineRenderer.cpp
@@ -7,36 +7,21 @@ namespace Spear
 
 	LineRenderer::LineRenderer()
 	{
-		const std::vector<GLfloat> instancePosData{
-			-0.5f, -1.f,	// line1 bottom
-			-0.5f, 1.f,		// line1 top
-			-0.5f, 0.1f,	// line2 A
-			0.5f, 0.1f,		// line2 B			
-			0.f, -0.5f,		// line3 A
-			0.f, 0.7f		// line3 B
-		};
-
-		const std::vector<GLfloat> instanceColorData{
-			1.f, 0.f, 0.f, 1.f,	// line 1
-			0.f, 1.f, 0.f, 0.8f,	// line 2
-			0.f, 0.f, 1.f, 1.f, // line 3
-		};
-
-
-		// Create VAO
+		// CREATE VAO
 		glGenVertexArrays(1, &m_vertexArrayObj);
 		glBindVertexArray(m_vertexArrayObj);
 
-		// Instanced Pos
+		// SETUP POS BUFFER
 		glGenBuffers(1, &m_instancePosBuffer);
 		glBindBuffer(GL_ARRAY_BUFFER, m_instancePosBuffer);
 		glBufferData(
 			GL_ARRAY_BUFFER,
-			instancePosData.size() * sizeof(GLfloat),
-			instancePosData.data(),
+			INSTANCE_POS_MAX * sizeof(GLfloat),
+			nullptr,
 			GL_STATIC_DRAW
 		);
 
+		// SETUP POS ATTRIB
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(
 			0,
@@ -48,16 +33,17 @@ namespace Spear
 		);
 		glVertexAttribDivisor(0, 1);
 
-		// Instanced Col
+		// SETUP COL BUFFER
 		glGenBuffers(1, &m_instanceColorBuffer);
 		glBindBuffer(GL_ARRAY_BUFFER, m_instanceColorBuffer);
 		glBufferData(
 			GL_ARRAY_BUFFER,
-			instanceColorData.size() * sizeof(GLfloat),
-			instanceColorData.data(),
+			INSTANCE_COL_MAX * sizeof(GLfloat),
+			nullptr,
 			GL_STATIC_DRAW
 		);
 
+		// SETUP COL ATTRIB
 		glEnableVertexAttribArray(1);
 		glVertexAttribPointer(
 			1,
@@ -69,7 +55,31 @@ namespace Spear
 		);
 		glVertexAttribDivisor(1, 1);
 
+		// COMPILE SHADER
 		m_shaderProgram = ShaderCompiler::CreateShaderProgram("../Shaders/LineVS.glsl", "../Shaders/LineFS.glsl");
+	}
+
+	void LineRenderer::AddLine(const LineData& line)
+	{
+		if (m_lineCount >= LINE_MAX)
+		{
+			LOG("WARNING: LineRenderer line count exceeded! Skipping lines");
+			return;
+		}
+
+		int posIndex{FLOATS_PER_POS * m_lineCount};
+		m_instancePosData[posIndex + 0] = line.start.x;
+		m_instancePosData[posIndex + 1] = line.start.y;
+		m_instancePosData[posIndex + 2] = line.end.x;
+		m_instancePosData[posIndex + 3] = line.end.y;
+
+		int colIndex{FLOATS_PER_COLOR * m_lineCount};
+		m_instanceColorData[posIndex + 0] = line.r;
+		m_instanceColorData[posIndex + 1] = line.g;
+		m_instanceColorData[posIndex + 2] = line.b;
+		m_instanceColorData[posIndex + 3] = line.alpha;
+
+		m_lineCount++;
 	}
 
 	void LineRenderer::Render()
@@ -82,15 +92,32 @@ namespace Spear
 		glClearColor(0.5f, 0.5f, 0.5f, 1.f);
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-		// RENDER
+		// SETUP VAO
 		glUseProgram(m_shaderProgram);
 		glBindVertexArray(m_vertexArrayObj);
-		
-		glEnable(GL_LINE_SMOOTH);
+
+		// REFRESH POS DATA
+		glBindBuffer(GL_ARRAY_BUFFER, m_instancePosBuffer);
+		glBufferSubData(
+			GL_ARRAY_BUFFER,
+			0,
+			PosDataSize(),
+			m_instancePosData
+		);
+
+		// UPLOAD COL DATA
+		glBindBuffer(GL_ARRAY_BUFFER, m_instanceColorBuffer);
+		glBufferSubData(
+			GL_ARRAY_BUFFER,
+			0,
+			ColDataSize(),
+			m_instanceColorData
+		);
+
+		//glEnable(GL_LINE_SMOOTH);
 		//glLineWidth(10.0f);
 
-		glDrawArraysInstanced(GL_LINES, 0, 2, 3); // 2 vertices per instance, 3 instances
-
+		GLCheck(glDrawArraysInstanced(GL_LINES, 0, 2, m_lineCount)); // 2 vertices per instance, m_lineCount instances
+		m_lineCount = 0;
 	}
-
 }
