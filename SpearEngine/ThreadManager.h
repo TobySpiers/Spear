3b4e2265226fa25f55aec7cdfa2a69f389195f14
TobySpiers <Spiers.Toby@gmail.com>
2024-06-23 17:52:43 +0100

    Multithreaded the raycaster... 60fps now possible for much higher resolutions

--- SpearEngine/ThreadManager.h
+++ SpearEngine/ThreadManager.h
@@ -3,10 +3,28 @@
 #include <functional>
 #include <thread>
 #include <mutex>
+#include <shared_mutex>
 #include <queue>
 
 namespace Spear
 {
+	// 'Task' refers to a function distributed across any number of threads
+	class TaskHandle
+	{
+
+	public:
+		void Initialise(int threads);
+		int RemainingThreads();
+		void DecrementRemainingThreads();
+		bool IsTaskComplete();
+		void WaitForTaskComplete();
+
+	private:
+		std::atomic<int> m_activeThreads;
+		std::shared_mutex m_mutex;
+		std::condition_variable_any m_conditionTaskComplete;
+	};
+
 	class ThreadManager
 	{
 	public:
@@ -17,8 +35,8 @@ namespace Spear
 		ThreadManager();
 		~ThreadManager();
 
-		void DispatchTask(const ThreadTask& task);
-		void DispatchTaskMulti(const ThreadTask& task, u32 taskInstances);
+		void DispatchTask(const ThreadTask& task, TaskHandle* taskStatus = nullptr);
+		void DispatchTaskDistributed(const ThreadTask& task, TaskHandle* taskStatus = nullptr, u32 taskInstances = THREAD_COUNT);
 
 	private:
 		static void WorkerThread(u8 threadId, ThreadManager* pThreadManager);
@@ -26,10 +44,11 @@ namespace Spear
 		struct QueuedTask
 		{
 			ThreadTask task;
+			TaskHandle* pTaskStatus;
 			u32 taskInstanceID;
 		};
 
-		static const uint32_t THREAD_COUNT{7};
+		static const uint32_t THREAD_COUNT{16};
 		std::mutex m_mutex;
 		std::queue<QueuedTask> m_queuedTasks;
 		std::condition_variable m_queuedTasksUpdated;
--- SpearGame/Raycaster.h
+++ SpearGame/Raycaster.h
@@ -16,8 +16,11 @@ struct RaycastParams
 {
 	float fieldOfView{ TO_RADIANS(75.f) };
 	float farClip{50};
-	int xResolution{300}; // affects performance for walls + floors + ceilings.
-	int yResolution{200}; // affects performance for floors + ceilings.
+	int xResolution{900}; // internal resolution for raycaster
+	int yResolution{600}; 
+	int threads{15};
+	int XResolutionPerThread() { return xResolution / threads; }; // intentional integer division
+	int YResolutionPerThread() { return yResolution / threads; };
 
 	// Used only for 2D top-down rendering. Scale 1 = 1 tile : 1 pixel.
 	float scale2D{ 75.f };
