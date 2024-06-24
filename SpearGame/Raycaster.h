48a0c7719673226a1b6eb5dd8f684438882faacc
TobySpiers <Spiers.Toby@gmail.com>
2024-06-24 22:42:08 +0100

    Rays continue DDA search after finding a hit, preventing objects incorrectly obscuring others (short walls no longer obscure tall walls, etc.) Expanded vertical rendering features, floors/ceilings can now extend and form walls above/below the playspace Updated sample level to incorporate these features

--- SpearGame/Raycaster.h
+++ SpearGame/Raycaster.h
@@ -19,11 +19,15 @@ struct RaycastParams
 	int xResolution{900}; // internal resolution for raycaster
 	int yResolution{600}; 
 	int threads{15};
+	int rayEncounterLimit{20}; // how many 'wall encounters' to allow per ray (used for rendering tall walls behind shorter walls)
 	int XResolutionPerThread() { return xResolution / threads; }; // intentional integer division
 	int YResolutionPerThread() { return yResolution / threads; };
 
 	// Used only for 2D top-down rendering. Scale 1 = 1 tile : 1 pixel.
 	float scale2D{ 75.f };
+
+	// Debug settings
+	bool highlightCorrectivePixels{ false }; // whether to render corrective pixels as BrightRed instead of using pixel-cloning
 };
 
 // class to cast and render rays
